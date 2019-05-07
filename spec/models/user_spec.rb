eb64f1633a132bf89d098bbc7894afe30abfe7ec
William Homer <billyhomeriii@gmail.com>
2019-05-07 15:02:04 -0600

    resolve test location for shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationship' do
+    {it should have_many :reviews}
+  end
 end
