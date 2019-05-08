dcdbc71770dbf36cc6ea90cdc91209b25102955b
William Homer <billyhomeriii@gmail.com>
2019-05-08 17:06:24 -0600

    add logic to sort by books review_average, page_count, number_reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,31 @@ class Book < ApplicationRecord
   def count_book_reviews
     reviews.count
   end
+
+  def self.sort_average_rating(order)
+    if order.upcase == 'ASC' || order.upcase == 'ASCENDING'
+      select('books.*, avg(reviews.rating) as average_rating').joins(:reviews).group('books.id').order('average_rating ASC')
+    elsif order.upcase == 'DESC' || order.upcase == 'DESCENDING'  
+      select('books.*, avg(reviews.rating) as average_rating').joins(:reviews).group('books.id').order('average_rating DESC')
+    end
+  end
+
+  def self.sort_number_pages(order)
+    if order.upcase == 'ASC' || order.upcase == 'ASCENDING'
+      select('books.*').order('books.page_count ASC')
+    elsif order.upcase == 'DESC' || order.upcase == 'DESCENDING'
+      select('books.*').order('books.page_count DESC')
+    end
+  end
+
+  def self.sort_number_reviews(order)
+    if order.upcase == 'ASC' || order.upcase == 'ASCENDING'
+      select('books.*, count(reviews.rating) as count_rating').joins(:reviews).group('books.id').order('count_rating ASC')
+    elsif order.upcase == 'DESC' || order.upcase == 'DESC'
+      select('books.*, count(reviews.rating) as count_rating').joins(:reviews).group('books.id').order('count_rating DESC')
+    end
+  end
 end
+
+# - sorted by number of reviews in ascending order
+# - sorted by number of reviews in descending order
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -9,9 +9,12 @@ RSpec.describe Book, type: :model do
     before :each do
       meg  = User.create!(username: 'Meg Griffen')
       @hp_prisoner = Book.create!(title: "Harry Potter and the Prisoner of Azkaban", page_count: 1234 , year_published: "2010-01-01" , image_url: "https://pics.cdn.librarything.com/picsizes/dd/b9/ddb96bc09edd9c6593241315251434f414f4141.jpg" )
+      @hp_sorcerer = Book.create!(title: "Harry Potter and the Sorcerer's Stone", page_count: 903 , year_published: "2019-05-08" , image_url: "https://pics.cdn.librarything.com/picsizes/bd/73/bd73e9ebf18b34a593551665267434f414f4141.jpg" )
       meg.reviews.create!(title: 'It was only OK', review: '1asdfasdfadsfadsfadfasdfa', rating: 4, book_id: @hp_prisoner.id)
       meg.reviews.create!(title: 'It was only sucked', review: '2asdfasdfadsfadsfadfasdfa', rating: 1, book_id: @hp_prisoner.id)
       meg.reviews.create!(title: 'It was only bad', review: '3asdfasdfadsfadsfadfasdfa', rating: 3, book_id: @hp_prisoner.id)
+      meg.reviews.create!(title: 'It was only OK', review: '1asdfasdfadsfadsfadfasdfa', rating: 3, book_id: @hp_sorcerer.id)
+      meg.reviews.create!(title: 'It was only sucked', review: '2asdfasdfadsfadsfadfasdfa', rating: 1, book_id: @hp_sorcerer.id)
     end
 
     it '.average_book_rating' do
@@ -20,6 +23,21 @@ RSpec.describe Book, type: :model do
 
     it '.count_book_reviews' do
       expect(@hp_prisoner.count_book_reviews).to eq(3)
+    end
+
+    it '.sort_average_rating' do
+      expect(Book.sort_average_rating('ASC')).to eq([@hp_sorcerer, @hp_prisoner]) 
+      expect(Book.sort_average_rating('DESC')).to eq([@hp_prisoner, @hp_sorcerer]) 
+    end
+
+    it '.sort_number_of_pages' do
+      expect(Book.sort_number_pages('ASC')).to eq([@hp_sorcerer, @hp_prisoner])
+      expect(Book.sort_number_pages('DESC')).to eq([@hp_prisoner, @hp_sorcerer])
+    end
+
+    it '.sort_number_of_reviews' do
+      expect(Book.sort_number_reviews('ASC')).to eq([@hp_sorcerer, @hp_prisoner])
+      expect(Book.sort_number_reviews('DESC')).to eq([@hp_prisoner, @hp_sorcerer])
     end
   end
 
