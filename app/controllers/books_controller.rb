cfa866aba51f26094f40394bdb4fd3a434cd60d6
Aurie <Myrdden42@gmail.com>
2019-05-11 17:00:39 -0600

    Fixed card arrangement in columns

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,18 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    #queryingstuff
+
+    books = Book.all
+    @left = []; @mid = []; @right = []
+    i = 0
+    books.each do |book|
+      case i
+      when 0; @left << book
+      when 1; @mid << book
+      when 2; @right << book
+      end
+      i += 1; i = 0 if i > 2
+    end
+    @backgrounds = ['#c00', '#c60', '#c90', '#3c0', '#39f', '#969', '#606']
   end
 end
