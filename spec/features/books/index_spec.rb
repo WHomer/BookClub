6cd44de479d362051a851f05749a35f844777c1f
Aurie <Myrdden42@gmail.com>
2019-05-11 19:04:42 -0600

    Add review and average tests (rudimentary)

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe "As a visitor," do
   describe "when visiting root," do
     before :each do
+      Book.destroy_all
+      Author.destroy_all
+      User.destroy_all
       @book1 = Book.create! title: "Book 1", page_count: "100", year_published: "10", image_url: "http://lorempixel.com/400/600/cats"
       @book2 = Book.create! title: "Book 2", page_count: "110", year_published: "20", image_url: "http://lorempixel.com/400/600/cats"
       @book3 = Book.create! title: "Book 3", page_count: "120", year_published: "30", image_url: "http://lorempixel.com/400/600/cats"
@@ -26,6 +29,13 @@ RSpec.describe "As a visitor," do
       @book3_author = "C"
       @book4_author = "D"
       @book5_author = "E"
+
+      @reviewinator = User.create! username: "Reviewinator"
+      @reviewinator.reviews.create! title: 'It was A book', review: 'hec', rating: 5, book_id: @book1.id
+      @reviewinator.reviews.create! title: 'It was B book', review: 'hecc', rating: 4, book_id: @book2.id
+      @reviewinator.reviews.create! title: 'It was C book', review: 'heccc', rating: 3, book_id: @book3.id
+      @reviewinator.reviews.create! title: 'It was D book', review: 'hecccc', rating: 2, book_id: @book4.id
+      @reviewinator.reviews.create! title: 'It was E book', review: 'heccccc', rating: 1, book_id: @book5.id
     end
 
     it "sees a title" do
@@ -74,7 +84,28 @@ RSpec.describe "As a visitor," do
     end
 
     it "sees average reviews and review count" do
-
+      visit "/"
+      
+      within("#book-content-#{@book1.id}") do
+        expect(page).to have_content(1)
+        expect(page).to have_content(5.0)
+      end
+      within("#book-content-#{@book2.id}") do
+        expect(page).to have_content(1)
+        expect(page).to have_content(4.0)
+      end
+      within("#book-content-#{@book3.id}") do
+        expect(page).to have_content(1)
+        expect(page).to have_content(3.0)
+      end
+      within("#book-content-#{@book4.id}") do
+        expect(page).to have_content(1)
+        expect(page).to have_content(2.0)
+      end
+      within("#book-content-#{@book5.id}") do
+        expect(page).to have_content(1)
+        expect(page).to have_content(1.0)
+      end
     end
   end
 end
