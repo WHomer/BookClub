29e2021801f27f882149a07cb473b5a95be96eb1
Aurie <Myrdden42@gmail.com>
2019-05-13 09:57:01 -0600

    Setup author index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :show]
-  resources :authors, only: [:show]
+  resources :authors, only: [:index, :show]
   resources :users, only: [:show]
   resources :reviews, only: [:destroy]
 end
